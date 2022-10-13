75bdfc40fce6efaada790763e698b786ea6b3276
mingyo0125 <wjdalsry125@gmail.com>
2022-10-13 20:26:42 +0900

    해상도 오류 미수정

---
+++ car_game2/Assets/Mingyo/Scripts/UI/SetActiveController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveController : MonoBehaviour
+{
+    [SerializeField] bool setActive;
+    [SerializeField] GameObject _gameObject;
+    public void SetActiveControl()
+    {
+        if(setActive == true)
+        {
+            _gameObject.SetActive(false);
+            setActive = false;
+            Debug.Log("false");
+        }
+        else
+        {
+            _gameObject.SetActive(true);
+            setActive = true;
+            Debug.Log("true");
+        }
+    }
+}
